6e51ca8e22ede0c0572c61ad356b16a32a69af84
raffayyy <muhammadrafay5165@gmail.com>
2024-07-29 15:40:36 +0500

    build(Navigation): Added Routes
    Created special paths and rendered elements for routing. Also installed related dependencies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
